e6273568a26d3fa17afeaa2612e2b56d04395c1a
camiperalta95 <peraltacamila861@gmail.com>
2024-04-19 22:01:46 -0300

    commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$Nombre = $GET['n'];
+$Apellido = $GET['a'];
+$Edad = $GET['e'];
+$Ciudad = $GET['c'];
+$Nacionalidad = $GET['nac'];
+
+
+echo $Nombre . ' Nombre <br>';
+echo $Apellido . ' Apellido <br>';
+echo $Edad  . ' Edad <br>';
+echo $Ciudad . 'Ciudad <br>';
+echo $Nacionalidad . ' Nacionalidad <br>';
+    
